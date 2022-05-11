09e956691b94e47e1da7bee3185f1d82deed449f
Ulises <robdiaz247@gmail.com>
2022-05-10 22:49:36 -0300

    JS 3

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
-var classbutt = document.getElementById("classbutton") 
-classbutt.onclick = seleccionar;
+/* VARIABLES */
 
-function seleccionar() {
+var classbutt = document.getElementById("classbutton") 
+var cardtyp = document.getElementById("cardtypes")
+
+/* FUNCIONES */
+cardtyp.onclick = error;
+classbutt.onclick = error;
+function error() {
     alert("Espera, aún no he añadido esa función.")
 }
